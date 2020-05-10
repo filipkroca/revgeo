a6c81fba596001b178b0177c9e7a1c93d0c425ef
filipkroca <filipkroca@gmail.com>
2020-05-10 14:44:45 +0200

    first commit

---
+++ revgeo.go
@@ -0,0 +1,8 @@
+// Copyright 2020 Filip Kroča. All rights reserved.
+// Use of this source code is governed by a MIT
+// license that can be found in the LICENSE file.
+
+// Package revgeo provides a reverse geocoding capability. Latitude and longitude to ISO country code.
+// This package uses GeoJSON polygons.
+// DATASET source: https://datahub.io/core/geo-countries#data-cli
+package revgeo
