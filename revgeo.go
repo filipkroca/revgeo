3660dc4b44494fb8c29c7a7215f3ffad7c8b35c4
filipkroca <filipkroca@gmail.com>
2020-05-11 14:45:44 +0200

    better performance

--- revgeo.go
+++ revgeo.go
@@ -24,12 +24,25 @@ const fileName = "countries.geojson.gz"
 
 // Decoder holds gemetry in memory and provides method Geocode()
 type Decoder struct {
-	fc *geojson.FeatureCollection
+	polygons []polygonWithIso
+	multiPolygons []multiPolygonWithIso
+}
+
+type polygonWithIso struct {
+	polygon orb.Polygon
+	iso string
+}
+
+type multiPolygonWithIso struct {
+	multiPolygon orb.MultiPolygon
+	iso string
 }
 
 func (d *Decoder) loadGeometry() {
 
-	
+	var polygons []polygonWithIso
+	var multiPolygons []multiPolygonWithIso
+
 	data, err := Asset("data/countries.geojson.gz")
 	if err != nil {
 		log.Panicln(err)
@@ -49,7 +62,21 @@ func (d *Decoder) loadGeometry() {
     log.Panic(err)
 	}
 
-	d.fc = featureCollection
+	for _, feature := range featureCollection.Features {
+		// Try on a MultiPolygon to begin
+		multiPoly, isMulti := feature.Geometry.(orb.MultiPolygon)
+		if isMulti {
+			multiPolygons = append(multiPolygons, multiPolygonWithIso{multiPoly, feature.Properties["ISO_A3"].(string)})
+		} else {
+				polygon, isPoly := feature.Geometry.(orb.Polygon)
+				if isPoly {
+					polygons = append(polygons, polygonWithIso{polygon, feature.Properties["ISO_A3"].(string)})
+				}
+		}
+	}
+
+	d.polygons = polygons
+	d.multiPolygons = multiPolygons
 	
 }
 
@@ -57,26 +84,41 @@ func (d *Decoder) loadGeometry() {
 func (d *Decoder) Geocode(lat float64, lng float64) (string, error) {
 
 	point := orb.Point{lat, lng}
-	if d.fc == nil {
+	if d.polygons == nil {
 		// load gemetry from GeoJSON
 		d.loadGeometry()
 	}
-	for _, feature := range d.fc.Features {
-			// Try on a MultiPolygon to begin
-			multiPoly, isMulti := feature.Geometry.(orb.MultiPolygon)
-			if isMulti {
-					if planar.MultiPolygonContains(multiPoly, point) {
-							return fmt.Sprintf("%v", feature.Properties["ISO_A3"]), nil
-					}
-			} else {
-					// Fallback to Polygon
-					polygon, isPoly := feature.Geometry.(orb.Polygon)
-					if isPoly {
-							if planar.PolygonContains(polygon, point) {
-								return fmt.Sprintf("%v", feature.Properties["ISO_A3"]), nil
-							}
-					}
-			}
+
+	for _, polygon := range d.polygons {
+		if planar.PolygonContains(polygon.polygon, point) {
+			return fmt.Sprintf("%v", polygon.iso), nil
+		}
 	}
+
+	for _, multiPolygon := range d.multiPolygons {
+		if planar.MultiPolygonContains(multiPolygon.multiPolygon, point) {
+			return fmt.Sprintf("%v", multiPolygon.iso), nil
+		}
+	}
+	
 	return "", fmt.Errorf("Unable to find country for lat: %v lng: %v", lat, lng)
 }
+
+/*
+
+	for _, feature := range d.fc.Features {
+		// Try on a MultiPolygon to begin
+		multiPoly, isMulti := feature.Geometry.(orb.MultiPolygon)
+		if isMulti {
+				if planar.MultiPolygonContains(multiPoly, point) {
+						return fmt.Sprintf("%v", feature.Properties["ISO_A3"]), nil
+				}
+		} else {
+				// Fallback to Polygon
+				polygon, isPoly := feature.Geometry.(orb.Polygon)
+				if isPoly {
+						if planar.PolygonContains(polygon, point) {
+							return fmt.Sprintf("%v", feature.Properties["ISO_A3"]), nil
+						}
+				}
+		}*/
